43ecf9205a79bab82ae4837d03adf7615abeb9ac
Krahoborov <ivaniv.pav.o@gmail.com>
2025-05-24 04:52:13 +0300

    main.py

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import logging
 from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    CallbackQueryHandler,
-    MessageHandler,
-    filters
+    ApplicationBuilder, CommandHandler, CallbackQueryHandler,
+    MessageHandler, ChatMemberHandler, filters
 )
-from config import TOKEN
 import handlers
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+# === Настройка логирования ===
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+# Снижаем уровень логов для httpx (подавляем INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+# Вставьте свой Telegram Token
+TELEGRAM_TOKEN = "7978941282:AAE4DDfhL37SnzjPA6iaghDxB_y5Yspzocs"
 
 def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+    application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-    app.add_handler(CommandHandler("start", handlers.start))
-    app.add_handler(CallbackQueryHandler(handlers.button_handler))
-    app.add_handler(CallbackQueryHandler(handlers.stop_callback_handler, pattern="^stop$"))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handlers.handle_message))
+    # Обработчики
+    application.add_handler(CommandHandler("start", handlers.start))
+    application.add_handler(CallbackQueryHandler(handlers.button_handler))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handlers.handle_message))
+    application.add_handler(ChatMemberHandler(handlers.handle_my_chat_member, ChatMemberHandler.MY_CHAT_MEMBER))
 
-    logger.info("Бот запущен...")
-    app.run_polling()
+    # Запуск бота
+    application.run_polling()
 
 if __name__ == "__main__":
     main()
