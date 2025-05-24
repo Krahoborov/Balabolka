707512249fa2a05f9e55fa406d5af1488f7f75c5
Krahoborov <ivaniv.pav.o@gmail.com>
2025-05-24 04:49:34 +0300

    config.py

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 # Токен бота и доступные каналы для выбора (ID канала : Название)
-TOKEN = '7541007648:AAEUdZO949aHmH3qs62doFGr6bf6txrp5HU'
+TOKEN = '7978941282:AAE4DDfhL37SnzjPA6iaghDxB_y5Yspzocs'
+
+# GorgeousVarya: 7978941282:AAE4DDfhL37SnzjPA6iaghDxB_y5Yspzocs
+# Kurbetvodnu: 7541007648:AAEUdZO949aHmH3qs62doFGr6bf6txrp5HU
+# Отключение копий ботов
+# curl -X POST "https://api.telegram.org/bot<токен>/deleteWebhook"
